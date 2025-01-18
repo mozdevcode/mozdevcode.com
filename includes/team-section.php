3f017dadf32380d694b4a9d5d1b6d93cce55234c
Félix Sumburane <felixpaulosumburane@gmail.com>
2025-01-18 15:33:36 +0200

    Sessao de servicos melhorada

--- includes/team-section.php
+++ includes/team-section.php
@@ -4,37 +4,10 @@
             <div class="col-lg-3 col-md-6">
                 <div class="team-member">
                     <div class="main-content">
-                        <img src="assets/images/member-04.jpg" alt="">
-                        <span class="category">Presidente</span>
-                        <h4></h4>
-                        <ul class="social-icons">
-                            <li><a href="#"><i class="zmdi zmdi-facebook"></i></a></li>
-                            <li><a href="#"><i class="zmdi zmdi-twitter"></i></a></li>
-                            <li><a href="#"><i class="zmdi zmdi-linkedin"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="team-member">
-                    <div class="main-content">
-                        <img src="assets/images/member-02.jpg" alt="">
-                        <span class="category">Vice-Presidente</span>
-                        <h4></h4>
-                        <ul class="social-icons">
-                            <li><a href="#"><i class="zmdi zmdi-facebook"></i></a></li>
-                            <li><a href="#"><i class="zmdi zmdi-twitter"></i></a></li>
-                            <li><a href="#"><i class="zmdi zmdi-linkedin"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="team-member">
-                    <div class="main-content">
-                        <img src="assets/images/member-03.jpg" alt="">
-                        <span class="category">Gestor de Comunidades</span>
-                        <h4></h4>
+                        <img src="assets/images/member-gen.jpg" alt="">
+                        <h4 style="margin: 0px;">Dercio Matsimbe</h4>
+                        <span class="category">Moderador</span>
+                        <br /><br />
                         <ul class="social-icons">
                             <li><a href="#"><i class="zmdi zmdi-facebook"></i></a></li>
                             <li><a href="#"><i class="zmdi zmdi-twitter"></i></a></li>
@@ -47,8 +20,43 @@
                 <div class="team-member">
                     <div class="main-content">
                         <img src="assets/images/member-gen.jpg" alt="">
-                        <span class="category">IT/Developer</span>
-                        <h4></h4>
+                        <h4 style="margin: 0px;">Salvador Mudaca</h4>
+                        <span class="category">Tech Lead</span>
+                        <br /><br />
+                        <ul class="social-icons">
+                            <li><a href="#"><i class="zmdi zmdi-facebook"></i></a></li>
+                            <li><a href="#"><i class="zmdi zmdi-twitter"></i></a></li>
+                            <li><a href="#"><i class="zmdi zmdi-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="team-member">
+                    <div class="main-content">
+                        <img draggable="false" src="assets/images/sumburane.jpg" alt="">
+                        <h4 style="margin: 0px;">Félix Sumburane</h4>
+                        <span class="category">Presidente</span>
+                        <br /><br />
+                        <ul class="social-icons">
+                            <li><a target="_blank" href="https://facebook.com/felixsumburane7"><i
+                                        class="zmdi zmdi-facebook"></i></a>
+                            </li>
+                            <li><a target="_blank" href="https://x.com/felixsumburane"><i
+                                        class="zmdi zmdi-twitter"></i></a></li>
+                            <li><a target="_blank" href="https://www.linkedin.com/in/felixsumburane/"><i
+                                        class="zmdi zmdi-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="team-member">
+                    <div class="main-content">
+                        <img src="assets/images/member-gen.jpg" alt="">
+                        <h4 style="margin: 0px;">Sebastião Web</h4>
+                        <span class="category">CTO</span>
+                        <br /><br />
                         <ul class="social-icons">
                             <li><a href="#"><i class="zmdi zmdi-facebook"></i></a></li>
                             <li><a href="#"><i class="zmdi zmdi-twitter"></i></a></li>
