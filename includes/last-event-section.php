66a13d1671d449077f65ce70d7f13ebf5b0877f8
Félix Sumburane <felixpaulosumburane@gmail.com>
2025-11-28 14:20:32 +0200

    update

--- includes/last-event-section.php
+++ includes/last-event-section.php
@@ -3,8 +3,6 @@
         <div class="row">
             <div class="col-lg-6 offset-lg-1">
                 <div class="accordion" id="accordionExample">
-                    <h1 style="color: white; font-weight: normal; text-align: left;">Último evento realizado</h1>
-                    <br />
                     <div class="accordion-item" style="padding: 20px;">
                         <h2 style="font-size: 1.2em;" class="accordion-header" id="headingOne">
                             Tema
